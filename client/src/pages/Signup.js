33894259befee7a4d5fa518d324a7f28b2dba1d8
Vue Lee <vuelee09@gmail.com>
2022-08-01 15:53:21 -0500

    implement authentication functionality

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 
 const Signup = () => {
@@ -28,7 +29,8 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState }
       });
-      console.log(data);
+      
+      Auth.login(data.addUser.token);
     } 
     catch (e) {
       console.error(e);
