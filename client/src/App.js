5958c0995cb3fc47a4cddbc85b21438d5ca5fbb1
Vue Lee <vuelee09@gmail.com>
2022-08-01 11:17:20 -0500

    install and setup apollo client, create thought component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const httpLink = createHttpLink({
+    uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
 function App() {
-  return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        // wrapping our JSX with the <ApolloProvider> element will enable our app to interact with our Apollo Client instance
+        <ApolloProvider client={client}>
+            <div className='flex-column justify-flex-start min-100-vh'>
+                <Header />
+                <div className='container'>
+                    <Home />
+                </div>
+                <Footer />
+            </div>
+        </ApolloProvider>
+    );
 }
 
 export default App;
