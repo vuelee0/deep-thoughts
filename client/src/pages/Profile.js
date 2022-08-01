dc1c93cf7323e414458345392766d316f9101a6e
Vue Lee <vuelee09@gmail.com>
2022-08-01 17:39:07 -0500

    create thought form component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,7 @@ import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { ADD_FRIEND } from '../utils/mutations';
+import ThoughtForm from '../components/ThoughtForm';
 
 
 const Profile = () => {
@@ -81,6 +82,8 @@ const Profile = () => {
                     />
                 </div>
             </div>
+
+            <div className="mb-3">{!userParam && <ThoughtForm />}</div>
         </div>
     );
 };
