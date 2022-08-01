d89ba99a74b6ed9498f4964dea87f72a4db0ded2
Vue Lee <vuelee09@gmail.com>
2022-07-31 21:03:22 -0500

    add and setup JWT

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -28,10 +29,11 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            const token = signToken(user);
 
-            return user;
+            return { token, user };
         },
-        
+
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
@@ -45,7 +47,8 @@ const resolvers = {
                 throw new AuthenticationError('Incorrect credentials');
             }
           
-            return user;
+            const token = signToken(user);
+            return { token, user };
         }
     }
 }
