c28a9e01ef1ffb3a45b43cd9bf1f399f894f5895
Vue Lee <vuelee09@gmail.com>
2022-08-01 16:49:56 -0500

    add user base queries

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
+
 
 const Profile = () => {
     const { username: userParam } = useParams();
 
-    const { loading, data } = useQuery(QUERY_USER, {
+    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
         variables: { username: userParam }
     });
 
-    const user = data?.user || {};
+    const user = data?.me || data?.user || {};
 
     if (loading) {
         return <div>Loading...</div>;
+    }
+
+    // navigate to personal profile page if username is the logged-in user's
+    if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+        return <Navigate to="/profile" />;
+    }
+
+    // redirects user with the below message if not logged in and viewing /profile
+    if (!user?.username) {
+        return (
+            <h4>
+                You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+            </h4>
+        );
     }
 
 
@@ -23,7 +39,7 @@ const Profile = () => {
         <div>
             <div className="flex-row mb-3">
                 <h2 className="bg-dark text-secondary p-3 display-inline-block">
-                    Viewing {user.username}'s profile.
+                    Viewing {userParam ? `${user.username}'s` : 'your'} profile.
                 </h2>
             </div>
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -60,3 +60,48 @@ export const QUERY_USER = gql`
         }
     }
 `;
+
+
+// retrieve essentially all data related to the logged-in user
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            friendCount
+            thoughts {
+                _id
+                thoughtText
+                createdAt
+                reactionCount
+                reactions {
+                    _id
+                    createdAt
+                    reactionBody
+                    username
+                }
+            }
+            friends {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+
+export const QUERY_ME_BASIC = gql`
+    {
+        me {
+            _id
+            username
+            email
+            friendCount
+            friends {
+                _id
+                username
+            }
+        }
+    }
+`;
